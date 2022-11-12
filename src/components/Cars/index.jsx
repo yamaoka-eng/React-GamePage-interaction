8d6c0d809de35207025db1a29a3fa12c6cb9a43f
yamaoka-eng <554209835@qq.com>
2022-11-12 21:08:52 +0800

    图片资源

--- src/components/Cars/index.jsx
+++ src/components/Cars/index.jsx
@@ -1,128 +1,212 @@
-import { useState, useEffect, useRef } from 'react'
-import { gsap } from "gsap"
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useState, useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { getImage, getMusic } from "../../utils";
 
-import './index.scss'
+import "./index.scss";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const carData = [
   {
     id: 1,
     name: "Destiny 2: Season of the Splicer",
-    platform: 'PlayStation 5 - Xbox Series X/S - PlayStation 4 - Xbox One - PC',
-    month: 'MAY',
-    day: '03',
-    role: 'destiny-role.png',
-    background: 'destiny-card.png',
-    music: 'sading.mp3'
+    platform: "PlayStation 5 - Xbox Series X/S - PlayStation 4 - Xbox One - PC",
+    month: "MAY",
+    day: "03",
+    role: "destiny-role.png",
+    background: "destiny-card.png",
+    music: "sading.mp3",
   },
   {
     id: 2,
     name: "Assassin's Creed Valhalla: Wrath of the Druids DLC",
-    platform: 'PlayStation 5 - Xbox Series X/S - PlayStation 4 - Xbox One - PC',
-    month: 'MAY',
-    day: '13',
-    role: 'reskident_role.png',
-    background: 'resident-card.png',
-    music: 'pig.mp3'
+    platform: "PlayStation 5 - Xbox Series X/S - PlayStation 4 - Xbox One - PC",
+    month: "MAY",
+    day: "13",
+    role: "reskident_role.png",
+    background: "resident-card.png",
+    music: "pig.mp3",
   },
   {
     id: 3,
-    name: 'Resident Evil Village',
-    platform: 'PlayStation 5 - Xbox Series X/S - PlayStation 4 - Xbox One - PC',
-    month: 'MAY',
-    day: '07',
-    role: 'rassassins-role.png',
-    background: 'assassins-card.png',
-    music: 'gun.mp3'
-  }
-]
+    name: "Resident Evil Village",
+    platform: "PlayStation 5 - Xbox Series X/S - PlayStation 4 - Xbox One - PC",
+    month: "MAY",
+    day: "07",
+    role: "rassassins-role.png",
+    background: "assassins-card.png",
+    music: "gun.mp3",
+  },
+];
 
-const Card = ({data}) => {
+const Card = ({ data }) => {
+  const el = useRef();
+  const audioEl = useRef();
 
-  const el = useRef()
-  const audioEl = useRef()
+  const q = gsap.utils.selector(el);
 
-  const q = gsap.utils.selector(el)
-  
-  const [ isScrollT, setIsScrollT ] = useState(false)
-  const [ isEnter, setIsEnter ] = useState(false)
-  const [ isLoading, setIsLoading ] = useState(false)
+  console.log(getImage(data.background));
 
-  const cardScrollTrigger = {trigger: ()=>q('.card-bg'), start: 'top top', end: '120% top', scrub: true}
+  const [isScrollT, setIsScrollT] = useState(false);
+  const [isEnter, setIsEnter] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(()=>{
-    gsap.fromTo(q('.card'), {x: 120, opacity: 0}, {x: 0, opacity: 1, stagger: 0.33, duration: 2, onStart: ()=> setIsLoading(true), onComplete: ()=> setIsLoading(false)})
-  },[])
+  const cardScrollTrigger = {
+    trigger: () => q(".card-bg"),
+    start: "top top",
+    end: "120% top",
+    scrub: true,
+  };
 
-  useEffect(()=>{
-    if (isEnter) return 
-    const animation1 = gsap.to(q('.card-bg'), {
-      rotateX: '45deg',
-      skewX: '-25deg',
-      transformOrigin: 'bottom',
+  useEffect(() => {
+    gsap.fromTo(
+      q(".card"),
+      { x: 120, opacity: 0 },
+      {
+        x: 0,
+        opacity: 1,
+        stagger: 0.33,
+        duration: 2,
+        onStart: () => setIsLoading(true),
+        onComplete: () => setIsLoading(false),
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (isEnter) return;
+    const animation1 = gsap.to(q(".card-bg"), {
+      rotateX: "45deg",
+      skewX: "-25deg",
+      transformOrigin: "bottom",
       duration: 1,
       onStart: () => setIsScrollT(true),
       onReverseComplete: () => setIsScrollT(false),
-      scrollTrigger: cardScrollTrigger
-    })
-    const animation2 = gsap.to(q('.card-role'), {x: '-5%', scale: 1.2, transformOrigin: "bottom", duration: 1, scrollTrigger: cardScrollTrigger})
-    const animation3 = gsap.to(q('.des'), {x: '10%', y: '30%', ease: 'ease-in', duration: 0.8, scrollTrigger: cardScrollTrigger})
+      scrollTrigger: cardScrollTrigger,
+    });
+    const animation2 = gsap.to(q(".card-role"), {
+      x: "-5%",
+      scale: 1.2,
+      transformOrigin: "bottom",
+      duration: 1,
+      scrollTrigger: cardScrollTrigger,
+    });
+    const animation3 = gsap.to(q(".des"), {
+      x: "10%",
+      y: "30%",
+      ease: "ease-in",
+      duration: 0.8,
+      scrollTrigger: cardScrollTrigger,
+    });
     return () => {
-      animation1.scrollTrigger.kill()
-      animation2.scrollTrigger.kill()
-      animation3.scrollTrigger.kill()
-    }
-  },[isEnter])
+      animation1.scrollTrigger.kill();
+      animation2.scrollTrigger.kill();
+      animation3.scrollTrigger.kill();
+    };
+  }, [isEnter]);
 
-  useEffect(()=>{
-    const animation1 = gsap.to(q('.btn'), { y: -100, opacity: 0, duration: 0.7, scrollTrigger: {
-      trigger: ()=>q('.card-bg'),
-      start: '35% top',
-      end: 'bootom center',
-      scrub: true
-    } })
-    const animation2 = gsap.fromTo(q('.time'), { opacity: 0, y: 100 }, { opacity: 1, y: 0, duration: 0.7, scrollTrigger: {
-      trigger: ()=>q('.card-bg'),
-      start: 'top center',
-      end: 'bootom center',
-    } })
+  useEffect(() => {
+    const animation1 = gsap.to(q(".btn"), {
+      y: -100,
+      opacity: 0,
+      duration: 0.7,
+      scrollTrigger: {
+        trigger: () => q(".card-bg"),
+        start: "35% top",
+        end: "bootom center",
+        scrub: true,
+      },
+    });
+    const animation2 = gsap.fromTo(
+      q(".time"),
+      { opacity: 0, y: 100 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.7,
+        scrollTrigger: {
+          trigger: () => q(".card-bg"),
+          start: "top center",
+          end: "bootom center",
+        },
+      }
+    );
     return () => {
-      animation1.scrollTrigger.kill()
-      animation2.scrollTrigger.kill()
-    }
-  },[])
+      animation1.scrollTrigger.kill();
+      animation2.scrollTrigger.kill();
+    };
+  }, []);
 
   const onEnter = () => {
-    if (isScrollT) return
-    if (isLoading) return
-    setIsEnter(true)
-    gsap.to(q('.card-bg'), {rotateX: '45deg', skewX: '-25deg', transformOrigin: 'bottom', duration: 1, onStart: () => setIsEnter(true) })
-    gsap.to(q('.card-role'), {x: '-5%', scale: 1.2, transformOrigin: "bottom", duration: 1})
-    gsap.to(q('.des'), {x: '10%', y: '30%', ease: 'ease-in', duration: 0.8})
-  }
+    if (isScrollT) return;
+    if (isLoading) return;
+    setIsEnter(true);
+    gsap.to(q(".card-bg"), {
+      rotateX: "45deg",
+      skewX: "-25deg",
+      transformOrigin: "bottom",
+      duration: 1,
+      onStart: () => setIsEnter(true),
+    });
+    gsap.to(q(".card-role"), {
+      x: "-5%",
+      scale: 1.2,
+      transformOrigin: "bottom",
+      duration: 1,
+    });
+    gsap.to(q(".des"), { x: "10%", y: "30%", ease: "ease-in", duration: 0.8 });
+  };
 
   const onLeaver = () => {
-    if (isScrollT) return
-    if (isLoading) return
-    setIsEnter(true)
-    gsap.to(q('.card-bg'), {rotateX: '0', skewX: '0', transformOrigin: 'bottom', duration: 1, onComplete: () => setIsEnter(false) })
-    gsap.to(q('.card-role'), {x: 0, scale: 1, duration: 1})
-    gsap.to(q('.des'), {x: '0', y: '0', duration: 0.8})
-  }
+    if (isScrollT) return;
+    if (isLoading) return;
+    setIsEnter(true);
+    gsap.to(q(".card-bg"), {
+      rotateX: "0",
+      skewX: "0",
+      transformOrigin: "bottom",
+      duration: 1,
+      onComplete: () => setIsEnter(false),
+    });
+    gsap.to(q(".card-role"), { x: 0, scale: 1, duration: 1 });
+    gsap.to(q(".des"), { x: "0", y: "0", duration: 0.8 });
+  };
 
   const pllayMusic = () => {
-    audioEl.current.play()
-  }
+    audioEl.current.play();
+  };
 
   return (
     <div className="section" ref={el}>
-      <div className="card" data-text={`url(../../assets/images/${data.background})`} onMouseEnter={onEnter} onMouseLeave={onLeaver} onClick={pllayMusic}>
-        <div className='card-shadow' style={{ background: `url(/src/assets/images/${data.background}) no-repeat`, backgroundSize: 'cover'}}></div>
-        <div className="card-role" style={{ background: `url(/src/assets/images/${data.role}) no-repeat`, backgroundSize: 'cover' }}>
-        </div>
-        <div className="card-bg" style={{ background: `url(/src/assets/images/${data.background}) no-repeat`, backgroundSize: 'cover'}}></div>
+      <div
+        className="card"
+        data-text={getImage(data.background)}
+        onMouseEnter={onEnter}
+        onMouseLeave={onLeaver}
+        onClick={pllayMusic}
+      >
+        <div
+          className="card-shadow"
+          style={{
+            background: `${getImage(data.background)}) no-repeat`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div
+          className="card-role"
+          style={{
+            background: `${getImage(data.role)} no-repeat`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div
+          className="card-bg"
+          style={{
+            background: `${getImage(data.role)} no-repeat`,
+            backgroundSize: "cover",
+          }}
+        ></div>
         <div className="time">
           <h1>{data.day}</h1>
           <span>{data.month}</span>
@@ -137,22 +221,19 @@ const Card = ({data}) => {
           </a>
         </div>
       </div>
-      <audio src={`/src/assets/music/${data.music}`} ref={audioEl}></audio>
+      <audio src={getMusic(data.music)} ref={audioEl}></audio>
     </div>
-  )
-}
+  );
+};
 
 const Cars = () => {
   return (
     <>
-      {
-        carData.map(item => <Card data={item} key={item.id}/>)
-      }
+      {carData.map((item) => (
+        <Card data={item} key={item.id} />
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default Cars
-
-
-
+export default Cars;
--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,1 +1,5 @@
-export const getImage = (imgUrl) => new URL(`/src/assets/images/${imgUrl}`, import.meta.url).href
+export const getImage = (imgUrl) =>
+  new URL(`/src/assets/images/${imgUrl}`, import.meta.url).href;
+
+export const getMusic = (musicUrl) =>
+  new URL(`/src/assets/music/${musicUrl}`, import.meta.url).href;
